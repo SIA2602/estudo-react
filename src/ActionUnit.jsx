2e1942eee6f4edae9529a0f99cf0e58fdccd79f6
Sia2602 <danilojose26@hotmail.com>
2021-07-18 17:28:49 -0300

    Substituindo Funcao por Classe

--- src/ActionUnit.jsx
+++ src/ActionUnit.jsx
@@ -1,13 +1,15 @@
+import React from 'react'
 import 'antd/dist/antd.css'
 import './index.css'
 
-function ActionUnit()
-{
-    return(                                      
-        document.getElementById("id01").style.display = "flex",           
-        document.getElementById("id02").style.display = "none",
-        document.getElementById("id03").style.display = "none"                
-    )       
+class ActionUnit extends React.Component{
+    render(){
+        return(                                      
+            document.getElementById("id01").style.display = "flex",           
+            document.getElementById("id02").style.display = "none",
+            document.getElementById("id03").style.display = "none"                
+        ) 
+    }          
 }
 
 export default ActionUnit
